524903dfcd20c684de0e5371eb24832267b7f492
Brain Mentors Pvt Ltd. <brainmentorspvtltd@gmail.com>
2024-07-22 00:31:47 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,21 @@
+// Java OOPS
+// what is class - class is a keyword
+// used to define a java class
+// class - it is a blue print / template / prototype
+// class name and file name should be same in Java
+// javac <SourcecodeFilename.java> --> Generate Byte Code
+// Byte Code (ClassName.class)
+// public - access outside the package (Folder)
+// JRE access outside from this folder
+
+
+class SecondClass{
+    // String args[] - Command Line Argument
+    // main is a function . Entry point
+    // void - no return 
+    // static - load when class is loaded, so no need to create object
+    //String args[] - Command Line Args - Can create ur own command
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
